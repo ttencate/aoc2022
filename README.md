b5a751952aeec9e1959bbb0376630bc3363b1dc4
Thomas ten Cate <ttencate@gmail.com>
2022-12-01 11:46:58 +0100

    Add framework and solve Day 1

---
+++ README.md
@@ -0,0 +1,1 @@
+These are my solutions to [Advent of Code 2022](https://adventofcode.com/2022).
---
+++ src/bin/01.rs
@@ -0,0 +1,30 @@
+fn run(input: &str) -> (u64, u64) {
+    let top3 = input
+        .split("\n\n")
+        .map(|elf| {
+            elf
+                .lines()
+                .map(|line| line.parse::<u64>().unwrap())
+                .sum()
+        })
+        .fold([0; 3], |mut top3, calories| {
+            if calories >= top3[0] {
+                top3[0] = calories;
+                top3.sort();
+            }
+            top3
+        });
+    (*top3.last().unwrap(), top3.iter().sum())
+}
+
+#[test]
+fn examples() {
+    assert_eq!(run(&aoc::example!(0)), (24000, 45000));
+}
+
+#[test]
+fn answer() {
+    assert_eq!(run(&aoc::input!()), (66616, 199172));
+}
+
+aoc::main!(run);
