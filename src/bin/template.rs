6bfbd1ec9680afd423aeec5cc8c44e2c73daec6c
Thomas ten Cate <ttencate@gmail.com>
2022-12-12 09:33:05 +0100

    Add template

---
+++ src/bin/template.rs
@@ -0,0 +1,15 @@
+fn run(input: &str) -> usize {
+    0
+}
+
+#[test]
+fn examples() {
+    assert_eq!(run(&aoc::example!(0)), 0);
+}
+
+#[test]
+fn input() {
+    assert_eq!(run(&aoc::input!()), 0);
+}
+
+aoc::main!(run);
